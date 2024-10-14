aa88d0762e2ff96de26da2dfa41d2a6a5f2f8d78
alysaphiraa <alyasaphira29@gmail.com>
2024-10-14 15:13:30 +0700

    Update dashboard1.py

--- Dashboard/dashboard1.py
+++ Dashboard/dashboard1.py
@@ -167,7 +167,7 @@ with col3:
 st.subheader('Monthly Rentals')
 fig, ax = plt.subplots(figsize=(24, 8))
 ax.plot(
-    monthly_rent_df['registered'],
+    monthly_rent_df['sum'],
     monthly_rent_df['count'],
     marker='o', 
     linewidth=2,
